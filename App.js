c6dc63365fd5a59eda4c311604b368d55a956939
jyan95 <jyan0602@gmail.com>
2019-09-20 15:25:51 -0400

    working on grid

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FixedDimensionsBasics from './containers/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>a yeet</Text>
+      <FixedDimensionsBasics/>
     </View>
   );
 }
---
+++ components/FullGrid.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StyleSheet, Text, View, Flatlist, Dimensions } from 'react-native';
+
+const data = [
+  // user's data here
+]
+
+const formatData = (data, numCols) => {
+  const numOfFullRows = Math.floow(data.length / numCols);
+  let numOfElsLastRow = data.length - (numOfFullRows * numCols);
+  while(numOfElsLastRow !== numCols && numOfElsLastRow !== 0) {
+    data.push( {key: `blank-${numOfElsLastRow}`, empty: true});
+
+  }
+  return data
+};
+
+const numCols = 3;
+
+export default class FullGrid extends React.Component {
+  renderItem = ({ item, index }) => {
+    if(item.empty === true) {
+      return <View style={[styles.item, styles.itemInvisible]} />;
+    }
+    return (
+      <View
+        style={styles.item}
+      >
+        <Text style={styles.itemText}>{item.key}</Text>
+      </View>
+    );
+  };
+
+  render() {
+    return(
+      <Flatlist
+        data={formatData(data, numCols)}
+        style={styles.container}
+        renderItem={this.renderItem}
+        numColumns={numCols}
+      />
+    );
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1 ,
+    marginVertical: 20,
+  },
+  item: {
+    backgroundColor: '#4D243D',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    margin: 1,
+    height: Dimensions.get('window').width / numCols,
+  },
+  itemInvisible: {
+    backgroundColor: 'transparent',
+  },
+  itemText: {
+    color: '#fff',
+  },
+});
