9687bcdd0ea276b248eab842d7d264fd823cfd65
jyan95 <jyan0602@gmail.com>
2019-09-23 15:38:21 -0400

    note component

---
+++ components/Note.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class Note extends Component {
+  render() {
+    return (
+      <View>
+        // code for editable textbox here
+      </View>
+    );
+  }
+}
