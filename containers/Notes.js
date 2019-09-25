191c1cbf437577269df0c4628803fb3edb4c3cca
jyan95 <jyan0602@gmail.com>
2019-09-25 13:05:51 -0400

    more skeleton

---
+++ containers/Notes.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class Notes extends Component {
+  render() {
+    return (
+      <View>
+        // note component
+      </View>
+    );
+  }
+}
---
+++ containers/Weeks.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class Weeks extends Component {
+  render() {
+    return (
+      <View>
+        // conditional render weeks grid (default to full life view)
+      </View>
+    );
+  }
+}
